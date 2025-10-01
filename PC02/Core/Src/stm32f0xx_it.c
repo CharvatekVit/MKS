6ec5c2548c25ad8c7bf9d30f41cede6d817cd903
CharvatekVit <246850@vutbr.cz>
2025-10-01 09:18:06 +0200

    PC02

--- PC02/Core/Src/stm32f0xx_it.c
+++ PC02/Core/Src/stm32f0xx_it.c
@@ -125,9 +125,9 @@ void PendSV_Handler(void)
 void SysTick_Handler(void)
 {
   /* USER CODE BEGIN SysTick_IRQn 0 */
-
+Tick++;
   /* USER CODE END SysTick_IRQn 0 */
-  HAL_IncTick();
+
   /* USER CODE BEGIN SysTick_IRQn 1 */
 
   /* USER CODE END SysTick_IRQn 1 */
